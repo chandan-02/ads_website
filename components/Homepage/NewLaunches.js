d1b6562440755d884d6d081bb1255c28e0098f09
Swapnil-101 <Swapniltiwari47@gmail.com>
2022-06-25 16:02:20 +0530

    last push

--- components/Homepage/NewLaunches.js
+++ components/Homepage/NewLaunches.js
@@ -4,12 +4,12 @@ import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 const NewLaunches = () => {
   const [click, setClick] = useState(1);
   return (
-    <div className="p-[1.5rem] mainborder">
+    <div className="2xl:p-[1.5rem] md:p-[1.5rem] xl:p-[1.5rem] py-[0.8rem] px-[0.8rem] mainborder">
       <div>
         <div className="flex gap-3">
           <img
             src="https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg"
-            className="2xl:w-[17rem] 2xl:h-[11rem] 2xl:rounded-[0.3rem] w-[10rem] h-[5rem] xl:w-[20rem] xl:h-[11rem]"
+            className="2xl:w-[15rem] 2xl:h-[9rem] 2xl:rounded-[0.3rem] w-[9rem] h-[6rem] xl:w-[12rem] xl:h-[8rem]"
           />
           <div className="w-full flex flex-col xl:gap-[0.4rem] 2xl:gap-[0.8rem] ">
             <div className="flex justify-between items-center w-full">
