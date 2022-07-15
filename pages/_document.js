8ef2b2344cde65511ba0cec4b596b46146f97f18
chandan-002 <chandan.unicoglobal@gmail.com>
2022-07-16 01:45:14 +0530

    analytics google added

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class _Document extends Document {
 
@@ -9,6 +10,19 @@ export default class _Document extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
+                    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-VPNSWW4DDQ"
+                        strategy="afterInteractive" />
+
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-VPNSWW4DDQ',{ page_path: window.location.pathname });
+                        `}
+                    </Script>
                 </Head>
                 <body>
                     <Main />
